dd03377d333bcf7bcb4c7e6399032c1622b8ac8a
Kristina <isitiriss@gmail.com>
2016-10-31 15:26:37 +0300

    crealet alert index

--- map-types.sh
+++ map-types.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -XPUT 'http://192.168.240.105:9200/_template/global_logs-template?pretty' -d @map-types.json \
+curl -XPUT 'http://192.168.240.105:9200/_template/alert-template?pretty' -d @map-types.json \
         --header "Content-Type: application/json"
 
 #curl -XPUT localhost:9200/_template/template_1?pretty -d  @map-types.json \
